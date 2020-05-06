ab9a36db8c91c80439c74cccb5b198a5aed65678
Nikita Bilous <nikita@bilous.me>
2020-05-06 15:34:43 +0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::fs::File;
-use std::io::{self, Lines};
-use std::path::PathBuf;
-use std::error;
+// use std::fs::File;
+// use std::io::{self, Lines};
+// use std::path::PathBuf;
+// use std::error;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::fmt;
-use std::collections::HashMap;
+// use std::fmt;
+use std::str::{self, FromStr};
+// use std::collections::HashMap;
 
 
 extern crate clap;
@@ -14,217 +15,107 @@ use clap::{Arg, App};
 extern crate fileinput;
 use fileinput::FileInput;
 
+extern crate pom;
+use pom::parser::*;
+use pom::char_class::*;
+// use pom::Parser;
+
 #[derive(Debug)]
-enum Token {
-    SquareOpen,
-    SquareClose,
+enum KrasValue {
     Str(String),
-    Colon,
-    FatComma, // =>
-    Comma,
-    Semicolon,
-    Equal,
-    BraceOpen,
-    BraceClose,
-    BracketOpen,
-    BracketClose,
-    Literal(String),
-    Space(String),
+    PairDelim(String),
+    ListDelim(String),
+    Delim(Box<KrasValue>),
+    ListItem(Box<(KrasValue, Option<KrasValue>)>),
+    List(Vec<KrasValue>),
+    Tuple(Vec<KrasValue>),
+    Ident(String),
+    Num(f64),
 }
 
-impl Token {
-    fn to_string(&self) -> String {
+fn pad(i: usize, l: usize) -> String {
+    std::iter::repeat(" ").take(i*l).collect()
+}
+
+impl KrasValue {
+    fn pretty(&self, ident: usize, level: usize) -> String {
         match self {
-            Token::SquareOpen => "[",
-            Token::SquareClose => "]",
-            Token::Str(ref s) => s,
-            Token::Colon => ":",
-            Token::FatComma => "=>",
-            Token::Comma => ",",
-            Token::Semicolon => ";",
-            Token::Equal => "=",
-            Token::BraceOpen => "{",
-            Token::BraceClose => "}",
-            Token::BracketOpen => "(",
-            Token::BracketClose => ")",
-            Token::Literal(ref s) => s,
-            Token::Space(ref s) => s,
-        }.to_string()
-    }
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{}", self.to_string())
-    }
-}
-
-struct Scanner {
-    input: Vec<char>,
-    tokens: Vec<Token>,
-    current: usize,
-    start: usize,
-}
-
-// http://craftinginterpreters.com/scanning.html
-impl Scanner {
-    fn new(s: &str) -> Self {
-        Scanner {
-            input: s.chars().collect(),
-            tokens: Vec::new(),
-            current: 0,
-            start: 0,
-        }
-    }
-
-    fn scan(&mut self) {
-        self.current = 0;
-        while ! self.at_end() {
-            self.start = self.current;
-            self.scan_token();
-        }
-    }
-
-    fn at_end(&self) -> bool {
-        self.current >= self.input.len()
-    }
-    
-    fn scan_token(&mut self) {
-        if self.at_end() {
-            return
-        }
-        let c = self.advance();
-        let token = match c {
-            '[' => Token::SquareOpen,
-            ']' => Token::SquareClose,
-            '{' => Token::BraceOpen,
-            '}' => Token::BraceClose,
-            '(' => Token::BracketOpen,
-            ')' => Token::BracketClose,
-            ':' => Token::Colon,
-            ',' => Token::Comma,
-            ';' => Token::Semicolon,
-            '=' => {
-                if self.check_match('>') {
-                    Token::FatComma
-                }
-                else {
-                    Token::Equal
-                }
-            },
-            '"' => self.string(),
-            _ if c.is_whitespace() => self.space(),
-            _ => self.literal(),
-            // _ => Token::(c.to_string()),
-        };
-        self.tokens.push(token);
-    }
-
-    fn space(&mut self) -> Token {
-        while ! self.at_end() && self.peek().is_whitespace() {
-            self.advance();
-        }
-        Token::Space(self.curr_string())
-    }
-
-    fn literal(&mut self) -> Token {
-        while ! self.at_end() {
-            let c = self.peek();
-            if c == '\\' && ! self.at_end() {
-                self.advance();
-                continue
-            }
-            if ! c.is_alphabetic() {
-                break
-            }
-            self.advance();
-        }
-        Token::Literal(self.curr_string())
-    }
-
-    fn string(&mut self) -> Token {
-        while ! self.at_end() {
-            let c = self.advance();
-            if c == '\\' && ! self.at_end() {
-                self.advance();
-                continue
-            }
-            if c == '"' {
-                break
-            }
-        }
-        if self.at_end() {
-            return Token::Literal(self.curr_string())
-        }
-        Token::Str(self.curr_string())
-    }
-
-    fn curr_string(&self) -> String {
-        self.input[self.start .. self.current].iter().collect()
-    }
-
-    fn check_match(&mut self, c: char) -> bool {
-        if self.at_end() {
-            false
-        }
-        else {
-            if self.input[self.current] == c {
-                self.current += 1;
-                true
-            }
-            else {
-                false
-            }
-        }
-    }
-
-    fn advance(&mut self) -> char {
-        // println!("{:?} [{}]", self.input, self.current);
-        self.current += 1;
-        self.input[self.current-1]
-    }
-
-    fn peek(&self) -> char {
-        self.input[self.current]
-    }
-}
-
-
-/*
-grammar
-expr = literal | array | dict | string | num 
-array = '[' items ']'
-items = items ',' | expr
-dict = '{' pairs '}'
-pairs = pairs ',' | pair
-pair = key (':' | '=>') expr
-key = literal | string | num
-
-*/
-
-enum Expr {
-    Literal(Token),
-    Array(Vec<Token>),
-    Dict(Box<HashMap<Token, Expr>>),
-}
-
-struct Parser {
-    tokens: Vec<Token>,
-}
-
-impl Parser {
-    fn new(tokens: Vec<Token>) -> Self {
-        Parser {
-            tokens: tokens,
-        }
-    }
-    fn parse(&self) {
-        for token in &self.tokens {
-            println!("{}", token)
+            KrasValue::Str(ref s) => s.to_string(),
+            KrasValue::List(ref v) => "[\n".to_string() 
+                + &v.iter().map(|x| x.pretty(ident, level+1)).collect::<String>() 
+                + "\n]",
+            KrasValue::PairDelim(ref s) => s.to_string(),
+            KrasValue::ListDelim(ref s) => s.to_string(),
+            KrasValue::ListItem(ref kv) =>  kv.0.pretty(ident, level) 
+                // + " "
+                + &kv.1.as_ref().map_or("".to_string(), |v| v.pretty(ident, level)),
+            KrasValue::Num(n) => n.to_string(),
+            KrasValue::Tuple(ref v) => "(".to_string() + &v.iter().map(|x| x.pretty(ident, level+1)).collect::<String>() + ")",
+            _ => format!("<<<< ??? {:?} ??? >>>>", self),
         }
     }
 }
 
+fn space<'a>() -> Parser<'a, u8, ()> {
+    one_of(b" \t\r\n").repeat(0..).discard()
+}
+
+fn ident<'a>() -> Parser<'a, u8, String> {
+    let first = is_a(alpha);
+    let rest = is_a(alphanum);
+    let ident = first + rest.repeat(0..);
+    ident.collect().convert(str::from_utf8).map(|s| s.to_string())
+}
+
+fn number<'a>() -> Parser<'a, u8, f64> {
+    let integer = one_of(b"123456789") - one_of(b"0123456789").repeat(0..) | sym(b'0');
+    let frac = sym(b'.') + one_of(b"0123456789").repeat(1..);
+    let exp = one_of(b"eE") + one_of(b"+-").opt() + one_of(b"0123456789").repeat(1..);
+    let number = sym(b'-').opt() + integer + frac.opt() + exp.opt();
+    number.collect().convert(str::from_utf8).convert(|s|f64::from_str(&s))
+}
+
+fn string<'a>() -> Parser<'a, u8, String> {
+    let special_char = sym(b'\\') | sym(b'/') | sym(b'"')
+        | sym(b'b').map(|_|b'\x08') | sym(b'f').map(|_|b'\x0C')
+        | sym(b'n').map(|_|b'\n') | sym(b'r').map(|_|b'\r') | sym(b't').map(|_|b'\t');
+    let escape_sequence = sym(b'\\') * special_char;
+    let string = sym(b'"') * (none_of(b"\\\"") | escape_sequence).repeat(0..) - sym(b'"');
+    string.convert(String::from_utf8)
+}
+
+fn pair_delim<'a>() -> Parser<'a, u8, KrasValue> {
+    let delim = space() * (seq(b":") | seq(b"=>")) - space();
+    delim.collect().convert(std::str::from_utf8).map(|s| KrasValue::PairDelim(s.to_string())) 
+}
+
+fn array_delim<'a>() -> Parser<'a, u8, KrasValue> {
+    let delim = space() * seq(b",") - space();
+    delim.collect().convert(std::str::from_utf8).map(|s| KrasValue::ListDelim(s.to_string()))
+}
+
+fn list_item<'a>() -> Parser<'a, u8, KrasValue> {
+    let delim = call(value) + (array_delim() | pair_delim()).opt();
+    delim.map(|(a, b)| KrasValue::ListItem(Box::new((a,b))))
+}
+
+fn array<'a>() -> Parser<'a, u8, Vec<KrasValue>> {
+    // let elems = list(call(list_item), delim);
+    let elems = list_item().repeat(0..);
+    sym(b'[') * space() * elems - sym(b']')
+}
+
+fn value<'a>() -> Parser<'a, u8, KrasValue> {
+    (
+        string().map(|s| KrasValue::Str(s))
+        | number().map(|n| KrasValue::Num(n))
+        | array().map(|arr| KrasValue::List(arr))
+    ) - space()
+}
+
+fn kras<'a>() -> Parser<'a, u8, KrasValue> {
+    space() * value() - end()
+}
 
 fn main() {
     let matches = App::new("Kras")
@@ -234,7 +125,7 @@ fn main() {
         .arg(Arg::with_name("indent")
              .short("i")
              .long("indent")
-             .help("indentation. 0 to disable (but stil color output)")
+             .help("indentation. 0 to disable (but still color output)")
              .default_value("4")
         )
         .arg(Arg::with_name("color")
@@ -260,24 +151,20 @@ fn main() {
         )
         .get_matches();
     let files = matches.values_of("input").map(|fs| fs.collect::<Vec<_>>()).unwrap_or(Vec::new());
-    // let files = if matches.is_present("input"){
-    //     matches.values_of("input").unwrap().collect::<Vec<_>>()
-    // }
-    // else {
-    //     vec![]
-    // };
     let input = FileInput::new(&files);
-    let mut reader = BufReader::new(input);
+    let reader = BufReader::new(input);
+    // let mut buf = Vec::new();
     for line in reader.lines() {
         match line {
             Ok(s) => {
-                let mut scanner = Scanner::new(&s);
-                println!("{}", s);
-                scanner.scan();
-                for t in scanner.tokens {
-                    print!(" <{}> ", t);
+                let buf = s.as_bytes().iter().map(|x| *x).collect::<Vec<u8>>();
+                // let buf = r#"["a": "c", ["b"]]"#.as_bytes().iter().map(|x| *x).collect::<Vec<u8>>();
+                // let buf = b"{}";
+                let r = kras().parse(&buf);
+                println!("{} ===>>> {:?}", s, r);
+                if let Ok(r) = r {
+                    println!("{}", r.pretty(4, 0))
                 }
-                println!("");
             }
             Err(err) => println!("{:?}", err),
         }
