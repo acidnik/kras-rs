c30154ce505c6f7d1f82fa29ae3628a36c02b5cd
Nikita Bilous <nikita@bilous.me>
2020-06-26 22:00:44 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@
 
 use std::io::BufReader;
 use std::io::BufRead;
-use std::str::{self, FromStr};
-use std::cmp::Ordering;
 use std::iter::FromIterator;
+use std::str::{self, FromStr};
 
 extern crate atty;
 
@@ -16,7 +15,6 @@ extern crate fileinput;
 use fileinput::FileInput;
 
 extern crate pom;
-use pom::parser::*;
 
 extern crate pretty;
 use pretty::*;
@@ -26,305 +24,12 @@ use pretty::termcolor::{Color, ColorChoice, ColorSpec, StandardStream};
 mod detect;
 use detect::*;
 
+mod pretty_value;
+use pretty_value::*;
 
-#[derive(Debug, Clone)]
-struct OrdF64(f64);
+mod parse;
+use parse::kras;
 
-impl PartialEq for OrdF64 {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl Eq for OrdF64 { }
-
-impl Ord for OrdF64 {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.0.partial_cmp(&other.0).unwrap_or(Ordering::Equal)
-    }
-}
-
-impl PartialOrd for OrdF64 {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-}
-
-#[derive(Debug, Clone, Ord, Eq, PartialEq, PartialOrd)]
-enum KrasValue {
-    // quoted string
-    Str((char, String)),
-
-    // value, delim?
-    ListItem((Box<KrasValue>, Option<String>)),
-
-    // value, delim, value, delim2?
-    Pair((Box<KrasValue>, String, Box<KrasValue>, Option<String>)),
-
-    // open brace, ListItem | Pair, close
-    List((String, Vec<KrasValue>, String)),
-
-    // a literal identificator, including null, true, false and any var name
-    Ident(String),
-
-    // ident, list
-    Constructor((Box<KrasValue>, Box<KrasValue>)),
-
-    // a number
-    Num(OrdF64),
-}
-
-impl KrasValue {
-    fn postprocess(mut self, sort: bool) -> Self {
-        // convert lists to dicts, sort
-        match self {
-            KrasValue::List((ref o, ref items, ref c)) => {
-                // if list looks like python dict / js object / perl hash / etc: convert ListItems
-                // to Pairs
-                // {key1: val1, key2: val2} => [ (key1, :), (val1, ,), (key2, :), (val2, ())] => [ (key1, :, val1, ,), (key2, :, val2, ()) ]
-                if items.len() % 2 == 0 {
-                    let mut is_dict = true;
-                    for (i, item) in items.iter().enumerate() {
-                        if i % 2 == 0 {
-                            // each even list item delimeter must be a dict separator
-                            if let KrasValue::ListItem((_, d)) = item {
-                                is_dict = match d {
-                                    Some(d) => {
-                                        d == "=>" || d == ":" || d == "="
-                                    },
-                                    None => {
-                                        false
-                                    },
-                                };
-                                if !is_dict {
-                                    break
-                                }
-                            }
-                        }
-                    }
-                    if is_dict {
-                        // TODO can it be done without clone?
-                        let mut res = Vec::new();
-                        for kv in items.chunks(2) {
-                            if let [k, v] = kv {
-                                println!("k = {:?}  v = {:?}", k, v);
-                                if let KrasValue::ListItem(k) = k {
-                                    if let KrasValue::ListItem(v) = v {
-                                        res.push(KrasValue::Pair(( 
-                                            Box::new(k.0.clone().postprocess(sort)),
-                                            k.1.clone().unwrap(),
-                                            Box::new(v.0.clone().postprocess(sort)),
-                                            v.1.clone()
-                                        )))
-                                    }
-                                }
-                            }
-                        }
-                        if sort {
-                            res.sort();
-                            self.fix_comma(&mut res);
-                        }
-                        self = KrasValue::List((o.to_string(), res, c.to_string()));
-                    }
-                }
-            },
-            KrasValue::Constructor(kv) => {
-                let (ident, mut args) = kv;
-                args = Box::new(args.postprocess(sort));
-                self = KrasValue::Constructor((ident, args))
-            }
-            _ => {},
-        }
-        self
-    }
-    fn fix_comma(&self, list: &mut Vec<KrasValue>) {
-        // {"2": 2, "1": 1} => sort => {"1": 1<no comma> "2": 2,<extra comma>} 
-        // => fix => {"1": 1,<add comma> "2": 2<remove comma> } => {"1": 1, "2": 2}
-        let len = list.len();
-        for (i, item) in list.iter_mut().enumerate() {
-            if let KrasValue::Pair((k, d, v, d2)) = item {
-                match (d2.is_some(), i == len-1) {
-                    (true, true) => {
-                        let d2 = None;
-                        *item = KrasValue::Pair((k.clone(), d.to_string(), v.clone(), d2))
-                    },
-                    (false, false) => {
-                        let d2 = Some(",".to_string());
-                        *item = KrasValue::Pair((k.clone(), d.to_string(), v.clone(), d2))
-                    }
-                    _ => {},
-                }
-            }
-        }
-    }
-}
-
-impl KrasValue {
-    fn kv_spaces(&self, d: String) -> RcDoc<ColorSpec> {
-        // '=>' - spaces around
-        // ':' - spaces to the right ': '
-        // '=' - no spaces
-        // ',' - ', '
-        let ds: &str = &d;
-        match ds {
-            "=>" => RcDoc::space().append(RcDoc::text(d)).append(RcDoc::space()),
-            ":"  => RcDoc::text(d).append(RcDoc::space()),
-            "="  => RcDoc::text(d),
-            ","  => RcDoc::text(d).append(RcDoc::space()),
-            _ => panic!(format!("unexpected kv delim ['{}']", d)),
-        }
-    }
-
-    fn to_doc(&self, indent: usize) -> RcDoc<ColorSpec> {
-        let nest = indent as isize; // why tf _i_size?
-        match self {
-            KrasValue::Str((q, s)) => RcDoc::as_string(q.to_string() + s + &q.to_string())
-                .annotate(ColorSpec::new().set_fg(Some(Color::Red)).clone()),
-            KrasValue::Ident(s) => RcDoc::as_string(s)
-                .annotate(ColorSpec::new().set_fg(Some(Color::Blue)).clone()),
-            KrasValue::List((op, it, cl)) => {
-                RcDoc::text(op)
-                    .annotate(ColorSpec::new().set_bold(true).clone())
-                    .append(RcDoc::nil()
-                        .append(RcDoc::line_())
-                        .nest(nest)
-                        .append(RcDoc::intersperse(it.iter().map(|x| x.to_doc(indent)), RcDoc::softline_())
-                            .nest(nest)
-                            // .append(Doc::line_())
-                        )
-                        .group()
-                    )
-                    .append(RcDoc::nil()
-                        .append(cl)
-                        .annotate(ColorSpec::new().set_bold(true).clone())
-                    )
-            },
-            KrasValue::Pair((k, d, v, d2)) => {
-                RcDoc::nil()
-                    .append(
-                        RcDoc::nil()
-                        // key
-                        .append(k.to_doc(indent))//.append(Doc::line_())
-                        // kv delim
-                        .append(self.kv_spaces(d.to_string()))
-                        .group()
-                    )
-                    .append(
-                        // value
-                        RcDoc::nil()
-                        .append(v.to_doc(indent))
-                        // list delim
-                        .append(d2.clone().map_or(RcDoc::nil(), |d| self.kv_spaces(d)))
-                        .group()
-                    )
-                    .append(Doc::line_())
-            },
-            KrasValue::ListItem((v, d)) => {
-                RcDoc::nil()
-                    .append(v.to_doc(indent))
-                    .append(d.clone().map_or(RcDoc::nil(), |d| self.kv_spaces(d)))
-            }
-            KrasValue::Num(OrdF64(n)) => RcDoc::as_string(n),
-            KrasValue::Constructor((id, args)) => {
-                RcDoc::nil()
-                    .append(id.to_doc(indent))
-                    .append(args.to_doc(indent))
-                    .group()
-            }
-        }
-    }
-}
-
-
-fn space<'a>() -> Parser<'a, char, ()> {
-    one_of(" \t\r\n").repeat(0..).discard()
-}
-
-fn ident<'a>() -> Parser<'a, char, String> {
-    let first = is_a(|c: char| c.is_alphabetic());
-    let dot = sym('.');
-    let rest = is_a(|c: char| c.is_alphanumeric()) | dot;
-    let ident = first + rest.repeat(0..);
-    ident.collect().map(String::from_iter)
-}
-
-fn number<'a>() -> Parser<'a, char, f64> {
-    let integer = (one_of("123456789") - one_of("0123456789").repeat(0..)) | sym('0');
-    let frac = sym('.') + one_of("0123456789").repeat(1..);
-    let exp = one_of("eE") + one_of("+-").opt() + one_of("0123456789").repeat(1..);
-    let number = sym('-').opt() + integer + frac.opt() + exp.opt();
-    number.collect().map(String::from_iter).convert(|s| f64::from_str(&s))
-}
-
-fn qqstring<'a>() -> Parser<'a, char, (char, String)> {
-    let special_char = sym('\\') | sym('/') | sym('"')
-        | sym('b').map(|_|'\x08') | sym('f').map(|_|'\x0C')
-        | sym('n').map(|_|'\n') | sym('r').map(|_|'\r') | sym('t').map(|_|'\t');
-    let escape_sequence = sym('\\') * special_char;
-    let string = sym('"') + (none_of("\\\"") | escape_sequence).repeat(0..) - sym('"');
-    string.map(|(a, b)| (a, b.iter().collect()))
-}
-
-fn qstring<'a>() -> Parser<'a, char, (char, String)> {
-    let special_char = sym('\\') | sym('/') | sym('"')
-        | sym('b').map(|_|'\x08') | sym('f').map(|_|'\x0C')
-        | sym('n').map(|_|'\n') | sym('r').map(|_|'\r') | sym('t').map(|_|'\t');
-    let escape_sequence = sym('\\') * special_char;
-    let string = sym('\'') + (none_of("\\\'") | escape_sequence).repeat(0..) - sym('\'');
-    string.map(|(a, b)| (a, b.iter().collect()))
-}
-
-fn string<'a>() -> Parser<'a, char, KrasValue> {
-    let string = qqstring() | qstring();
-
-    string.map(|(a, b)| KrasValue::Str((a, b)))
-}
-
-fn pair_delim<'a>() -> Parser<'a, char, String> {
-    let delim = space() * (seq(&[':']) | seq(&['=','>']) | seq(&['='])) - space();
-    delim.map(String::from_iter)
-}
-
-fn array_delim<'a>() -> Parser<'a, char, String> {
-    let delim = space() * sym(',') - space();
-    delim.map(|c| c.to_string())
-}
-
-fn list_item<'a>() -> Parser<'a, char, KrasValue> {
-    let delim = call(inner_value) + (array_delim() | pair_delim()).opt();
-    delim.map(|(a, b)| KrasValue::ListItem((Box::new(a), b)))
-}
-
-fn array<'a>() -> Parser<'a, char, (String, Vec<KrasValue>, String)> {
-    // parse array | set | dict | tuple | etc
-    // (a=>b, c => d) => [ (a=>) (b,) (c=>) (d) ]
-    let arr = sym('[') + space() * list_item().repeat(0..) + sym(']');
-    let set = sym('{') + space() * list_item().repeat(0..) + sym('}');
-    let tup = sym('(') + space() * list_item().repeat(0..) + sym(')');
-    (arr | set | tup).map(|((a, b), c)| (a.to_string(), b, c.to_string() ))
-}
-
-fn constructor<'a>() -> Parser<'a, char, KrasValue> {
-    let res = ident() - space() + array();
-    res.map(|(a, b)| KrasValue::Constructor((Box::new(KrasValue::Ident(a)), Box::new(KrasValue::List(b)))))
-}
-
-fn inner_value<'a>() -> Parser<'a, char, KrasValue> {
-    value() | ident().map(KrasValue::Ident)
-}
-
-fn value<'a>() -> Parser<'a, char, KrasValue> {
-    (
-        string()
-        | number().map(|n| KrasValue::Num(OrdF64(n)))
-        | constructor()
-        | array().map(|(s, arr, c)| KrasValue::List((s, arr, c)))
-    ) - space()
-}
-
-fn kras<'a>() -> Parser<'a, char, KrasValue> {
-    space() * value() - end()
-}
 
 fn main() {
     let matches = App::new("Kras")
@@ -388,13 +93,17 @@ fn main() {
                 let buf = s.chars().collect::<Vec<_>>();
                 let mut start = 0;
                 for (pos, data) in DetectDataIter::new(&buf) {
+                    // println!("detect //////// {} //////////", String::from_iter(data));
                     let r = kras().parse(data);
-                    // println!("{} ===>>> {:?}", s, r);
                     if let Ok(mut r) = r {
                         print!("{}", String::from_iter(buf[start..pos].iter()));
                         start = pos + data.len();
                         r = r.postprocess(sort);
+                        // println!("{} ===>>> {:?}", s, r);
                         r.to_doc(indent).render_colored(min_len, StandardStream::stdout(color_choice)).unwrap();
+                    }
+                    else {
+                        // println!("err {:?}", r);
                     }
                 }
                 println!("{}", String::from_iter(buf[start..].iter()));
@@ -403,3 +112,5 @@ fn main() {
         }
     }
 }
+// TODO test this
+// fn to_doc(&self, indent: usize) -> RcDoc<ColorSpec> { let nest = indent as isize; // why tf _i_size?  match self { KrasValue::Str((q, s)) => RcDoc::as_string(q.to_string() + s + &q.to_string()) .annotate(ColorSpec::new().set_fg(Some(Color::Red)).clone()), KrasValue::Ident(s) => RcDoc::as_string(s) .annotate(ColorSpec::new().set_fg(Some(Color::Blue)).clone()), KrasValue::List((op, it, cl)) => { RcDoc::text(op) .annotate(ColorSpec::new().set_bold(true).clone()) .append(RcDoc::nil() // .append(RcDoc::text("%")) .append(RcDoc::line_()) .nest(nest) // .append(RcDoc::intersperse(it.iter().map(|x| x.to_doc(indent)), RcDoc::softline_()) //     .nest(nest) //     .append(Doc::line_()) // ) .append(RcDoc::intersperse(it.iter().map(|x| x.to_doc(indent)), RcDoc::nil()) .nest(nest) // .group() .append(Doc::line_())) // .group()) .append(RcDoc::nil() .append(cl) .annotate(ColorSpec::new().set_bold(true).clone())) }, KrasValue::Pair((k, d, v, d2)) => { RcDoc::nil() .append( RcDoc::nil() // key .append(k.to_doc(indent))//.append(Doc::line_()) // kv delim .append(self.kv_spaces(d.to_string())) // .group()) .append( // value RcDoc::nil() .append(v.to_doc(indent)) // list delim .append(d2.clone().map_or(RcDoc::nil(), |d| self.kv_spaces(d))) // .group()) // .group() .append(RcDoc::text("⏎")) .append(RcDoc::softline_()) }, KrasValue::ListItem((v, d)) => { RcDoc::nil() .append(v.to_doc(indent)) .append(d.clone().map_or(RcDoc::nil(), |d| self.kv_spaces(d))) .append(RcDoc::softline_()) } KrasValue::Num(OrdF64(n)) => RcDoc::as_string(n), KrasValue::Constructor((id, args)) => { RcDoc::nil() .append(id.to_doc(indent)) .append(args.to_doc(indent)) // .group() } }.group() }
